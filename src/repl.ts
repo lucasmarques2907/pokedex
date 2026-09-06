1c6e69fbad79161cd0157b3cc59aaffe0bb276fd
lucasmarques2907 <lucasredra@gmail.com>
2026-09-05 22:39:06 -0300

    feat: REPL structure

---
+++ src/repl.ts
@@ -0,0 +1,50 @@
+import { commandExit } from "./command_exit.js";
+import { createInterface } from "readline";
+import { getCommands } from "./commands.js";
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    const commands = getCommands();
+    const cmd = commands[commandName];
+    
+    if (!cmd) {
+      console.log(
+        `Unknown command: ${commandName}`
+      );
+      rl.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(commands);
+    } catch (e) {
+      console.log(e);
+    }
+
+    rl.prompt();
+  });
+}
+
+export function cleanInput(input: string): string[] {
+  return input
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+}
