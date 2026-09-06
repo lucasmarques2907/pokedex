84224043b1d0c4a04672042310542a7372c46306
lucasmarques2907 <lucasredra@gmail.com>
2026-09-06 20:57:33 -0300

    refactor: implementing state

--- src/repl.ts
+++ src/repl.ts
@@ -1,43 +1,35 @@
-import { commandExit } from "./command_exit.js";
-import { createInterface } from "readline";
-import { getCommands } from "./commands.js";
+import { State } from "./state.js";
 
-export function startREPL() {
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "Pokedex > ",
-  });
+export function startREPL(state: State) {
+  state.readline.prompt();
 
-  rl.prompt();
-
-  rl.on("line", async (input) => {
+  state.readline.on("line", async (input) => {
     const words = cleanInput(input);
     if (words.length === 0) {
-      rl.prompt();
+      state.readline.prompt();
       return;
     }
 
     const commandName = words[0];
 
-    const commands = getCommands();
+    const commands = state.commands;
     const cmd = commands[commandName];
     
     if (!cmd) {
       console.log(
         `Unknown command: ${commandName}`
       );
-      rl.prompt();
+      state.readline.prompt();
       return;
     }
 
     try {
-      cmd.callback(commands);
+      cmd.callback(state);
     } catch (e) {
       console.log(e);
     }
 
-    rl.prompt();
+    state.readline.prompt();
   });
 }
 
